095269e4afc21c13d8c4dd2c9a56e4e4590544b8
xeniabarreto <xeniabarreto22@gmail.com>
2022-12-06 01:03:00 -0300

    Add Admin with TDD

---
+++ __tests__/admin.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+const Admin = require('../src/Admin');
+const Person = require('../src/Person');
+
+describe('Admin class test', () => {
+
+  it('should instantiate the Admin is done correctly', () => {
+    const admin = new Admin();
+    expect(admin instanceof Person).toBe(true);
+  });
+
+  it('should register admin with valid data', () => {
+    const admin = new Admin();
+    //expect(admin.body).toEqual('Adm successfully registered!');
+    expect(admin.registerAdmin('Mauricio', '338338338', 'teste@teste', '1234','true')).toBe('Adm successfully registered!');
+  });
+
+  it('should return error for registration with invalid data', () => {
+    const admin = new Admin();
+    expect(() => admin.registerAdmin('Mauricio', '', '123456', true)).toThrow('Error! Invalid data.');
+  });
+
+});
