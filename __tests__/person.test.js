f83f0a2ee2a069a8cd87feb64bdda33f59cb7a69
xeniabarreto <xeniabarreto22@gmail.com>
2022-12-06 22:18:40 -0300

    Add Tdd get cpf number

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-undef */
 const Person = require('../src/Person');
 
+const person = new Person();
+const person1 = new Person('Jose', '625658958-20', 'jose@hoo.com');
+
 describe('Person class test', () => {
   it('should instantiate the Person is done correctly', () => {
-    const person = new Person();
     expect(person instanceof Person).toBe(true);
   });
+
+  it('should return cpf number', () => {
+    expect(person1.cpf).toEqual('625658958-20');
+    expect(person1 instanceof Person).toBe(true); 
+  });
+
 });
