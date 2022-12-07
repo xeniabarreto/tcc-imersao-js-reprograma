d24959c821c15d506b5c2080244f66684bec0fa0
xeniabarreto <xeniabarreto22@gmail.com>
2022-12-06 22:59:16 -0300

    Created Medicine Superclass

---
+++ src/Medicine.js
@@ -0,0 +1,45 @@
+class Medicine {
+  name;
+  composition;
+  batch;
+  manufactured;
+  expiration;
+  retain_revenue;
+ 
+  constructor(name, composition, batch, manufactured, expiration, retain_revenue){
+    this.name = name;
+    this.composition = composition;
+    this.batch = batch;
+    this.manufactured = manufactured;
+    this.expiration = expiration;
+    this.retain_revenue = retain_revenue;
+  }
+}
+  
+module.exports = Medicine;
+  
+
+
+// {
+//     dipirona: 10,
+//     dipirona: 5,
+
+
+//     neosaldina: 11,
+// }
+
+
+//     dipirona : 10
+// }
+
+
+
+// entrada( nome, 1)
+// verificar
+// if retirar propierdade estatuca
+
+// retirada(nome, 1)
+// verificar
+// return MessageChannel
+
+// else 
