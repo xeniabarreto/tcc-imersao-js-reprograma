5a948d7a92144d9d2e9c7f3f7fe7e404bf2e7a9b
xeniabarreto <xeniabarreto22@gmail.com>
2022-12-09 18:25:27 -0300

    Add Pharmacy TDD

--- __tests__/pharmacy.test.js
+++ __tests__/pharmacy.test.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-undef */
-const Pharmacy = require('../src/Pharmacy');
+const Pharmacy = require('../src/services/pharmacy/Pharmacy');
 
-const pharmacy = new Pharmacy();
 
 describe('Pharmacy class test', () => {
-  it('should instantiate the Pharmacy is done correctly', () => {
+  it('should instantiate the Pharmacy', () => {
+    const pharmacy = new Pharmacy('12.546.658/0001-52', 'Royal', 'Autonomist Avenue - SP', '11 98112-8556', 'att@royal.com', 'Isaac', '258658952-58' );  
     expect(pharmacy instanceof Pharmacy).toBe(true);
   });
+
+  it('should return error when instantiating the Pharmacy due to incorrect data', () => {
+    expect(() => (new Pharmacy())).toThrow('Insert the data as CPF/CNPJ, email or telephone, correctly.');
+  });
+
 });
