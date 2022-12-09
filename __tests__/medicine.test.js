103bb126b9fddfe7c6cc04a17c130dae3d235653
xeniabarreto <xeniabarreto22@gmail.com>
2022-12-09 18:48:43 -0300

    Add tdd of medicines

--- __tests__/medicine.test.js
+++ __tests__/medicine.test.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-undef */
-const Medicine = require('../src/Medicine');
-
-const medicine = new Medicine();
+const Medicine = require('../src/services/medicine/Medicine');
 
 describe('Medicine class test', () => {
   it('should instantiate the Medicine is done correctly', () => {
+    const medicine = new Medicine('Tylenol', 'Paracetamol 500mg', '156625', '08/23');
     expect(medicine instanceof Medicine).toBe(true);
   });
+
+  it('should return the error when instantiating missing data', () => {
+    expect(() => (new Medicine())).toThrow('All data is required.');
+  });
+
 });
