a4a3215d380ce9a973442237de94ab486a266d63
xeniabarreto <xeniabarreto22@gmail.com>
2022-12-09 22:38:32 -0300

    Add HerbalMedicine Class

---
+++ src/services/medicine/HerbalMedicine.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-setter-return */
+const Medicine = require('./Medicine');
+
+class HerbalMedicine extends Medicine {
+  constructor(name, composition, batch, expiration, type, laboratory, value, qty) {
+    super(name, composition, batch, expiration);
+    this.type = type;
+    this.laboratory = laboratory;
+    this.value = value;
+    this.qty = qty;
+
+    this.constructor.herbalMedicineList.push({
+      name: this.name,
+      composition: this.composition,
+      batch: this.batch,
+      expiration: this.expiration,
+      type: this.type,
+      laboratory: this.laboratory,
+      value: this.value,
+      qty: this.qty,
+    });
+  }
+
+  static herbalMedicineList = [];
+
+  addMedicine(medicine, qty){
+    if(medicine instanceof HerbalMedicine){
+
+      if(typeof qty !== 'string' ){
+
+        if( qty >= 1  ){
+          return (medicine.qty += qty);
+        
+        } else {
+          throw new Error('It must be greater than 0.');
+        }
+      
+      } else {
+        throw new Error('It must be a number.');
+      }
+    
+    } else {
+      throw new Error('It must be instanced by HerbalMedicine.');
+    }
+  }
+
+  removeMedicine(medicine, qty) {
+    if (medicine instanceof HerbalMedicine) {
+      
+      if (typeof qty !== 'string') {
+        
+        if (medicine.qty > 0 && medicine.qty > qty) {
+          return medicine.qty -= qty;
+        
+        } else {
+          throw new Error('There are not medications available.');
+        }
+      
+      } else {
+        throw new Error('It must be a number.');
+      }
+    
+    } else {
+      throw new Error('It must be instanced by HerbalMedicine.');
+    }
+  }
+}
+
+
+module.exports = HerbalMedicine;
